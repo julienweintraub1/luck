aca3f8e0de706fd6553cfd5bdb0773921b75c5fc
Julien <weintraubjulien@gmail.com>
2024-01-15 19:32:02 -0500

    Display player data on the app

--- lib/player_service.dart
+++ lib/player_service.dart
@@ -1,17 +1,11 @@
 import 'dart:convert';
-import 'package:http/http.dart' as http;
+import 'package:flutter/services.dart';
 import 'player.dart';
 
 class PlayerService {
   Future<List<Player>> fetchPlayers() async {
-    final url = 'YOUR_JSON_URL'; // Replace with your JSON URL
-    final response = await http.get(Uri.parse(url));
-
-    if (response.statusCode == 200) {
-      List<dynamic> playerJson = json.decode(response.body);
-      return playerJson.map((json) => Player.fromJson(json)).toList();
-    } else {
-      throw Exception('Failed to load players');
-    }
+    final jsonString = await rootBundle.loadString('assets/player.json');
+    final List<dynamic> playerJsonList = json.decode(jsonString) as List;
+    return playerJsonList.map((json) => Player.fromJson(json)).toList();
   }
 }
